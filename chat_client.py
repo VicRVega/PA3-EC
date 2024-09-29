2307498fb1aa2112bc99e57953fb4a6995754841
VicRVega <vicramirez@csumb.edu>
2024-09-29 13:23:44 -0700

    main

---
+++ chat_client.py
@@ -0,0 +1,84 @@
+#!env python
+
+"""Chat client for CST311 Programming Assignment 3 - Extra Credit """
+__author__ = "[Group 4]"
+__credits__ = [
+    "Chris Tangonan",
+    "Edward Torres",
+    "Victoria Ramirez",
+    "Guillermo Zendejas"
+]
+
+# Import statements
+import socket as s
+import threading
+
+# Configure logging
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+# Set global variables
+server_name = '10.0.0.1' # should we set to have node h1 running server code correct?
+server_port = 12000
+
+# Add handler for a thread that receives incoming messages from server and prints them
+def incoming_message_handler(client_socket):
+    while True:
+        try:
+            incoming_message = client_socket.recv(1024)
+            if incoming_message:
+                # prints incoming message from server with other client's name concatenated at the front
+                print(incoming_message.decode())
+            else:
+                break
+        except:
+            break
+
+def main():
+    # Create socket
+    client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    try:
+        # Establish TCP connection
+        client_socket.connect((server_name,server_port))
+
+        # Create a thread that receives incoming messages from server and prints them
+        threading.Thread(target=incoming_message_handler, args=(client_socket,)).start()
+    except Exception as e:
+        log.exception(e)
+        log.error("***Advice:***")
+        if isinstance(e, s.gaierror):
+            log.error("\tCheck that server_name and server_port are set correctly.")
+        elif isinstance(e, ConnectionRefusedError):
+            log.error("\tCheck that server is running and the address is correct")
+        else:
+            log.error("\tNo specific advice, please contact teaching staff and include text of error and code.")
+        exit(8)
+
+    # Initial greeting and request for input from user
+    user_input = input('Welcome to the chat! To send a message, type it and press enter.\n')
+
+    try:
+        #keep the connection open as long as the message is not 'bye'
+        while user_input!='bye':
+            # Set data across socket to server
+            #  Note: encode() converts the string to UTF-8 for transmission
+            client_socket.send(user_input.encode())
+
+            # Get input from user
+            user_input = input()
+
+    finally:
+        #set user_input to 'bye' so server recieves final message
+        user_input = 'bye'
+        # Set data across socket to server
+        #  Note: encode() converts the string to UTF-8 for transmission
+        client_socket.send(user_input.encode())
+        # Close socket prior to exit
+        client_socket.close()
+
+# This helps shield code from running when we import the module
+if __name__ == "__main__":
+    main()
---
+++ chat_server.py
@@ -0,0 +1,104 @@
+#!env python
+
+"""Chat server for CST311 Programming Assignment 3 - Extra Credit"""
+__author__ = "[Group 4]"
+__credits__ = [
+    "Chris Tangonan",
+    "Edward Torres",
+    "Victoria Ramirez",
+    "Guillermo Zendejas"
+]
+
+
+import socket as s
+import time
+import threading
+
+# Configure logging
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+server_port = 12000
+thread_list = []
+connections = []
+clientXName = "Client X"
+clientYName = "Client Y"
+connection_socketX = None
+connection_socketY = None
+addresses = []
+
+def connection_handler(connection_socket, address):
+    global connection_socketX, connection_socketY
+
+    while True:
+        #  Read data from the new connection socket
+        #  Note: if no data has been sent this blocks until there is data
+        incoming_message = connection_socket.recv(1024)
+
+        # Creating local references for the other client's socket and string name
+        otherClientSocket, otherClientName = (connection_socketX, clientXName) if address[0] == "10.0.0.3" else (connection_socketY, clientYName)
+
+        # Decode data from UTF-8 bytestream
+        incoming_decoded = otherClientName + ": " + incoming_message.decode()
+        if incoming_decoded == otherClientName + ": " + 'bye':
+            break
+
+        # Log query information
+        log.info("Received query test \"" + str(incoming_decoded) + "\"" + "from: " + str(address))
+
+
+        # # Perform some server operations on data to generate response
+        # # time.sleep(10)
+        # response = query_decoded.upper()
+
+        log.info("Other socket is: \nconnection_socket: " + str(otherClientSocket))
+
+        forwarded_message = incoming_decoded.encode()
+
+        # Sent response over the network, encoding to UTF-8
+        otherClientSocket.send(forwarded_message)
+
+    # Close client socket
+    connection_socket.close()
+
+
+def main():
+    global connection_socketX, connection_socketY
+
+    # Create a TCP socket
+    # Notice the use of SOCK_STREAM for TCP packets
+    server_socket = s.socket(s.AF_INET,s.SOCK_STREAM)
+
+    # Assign port number to socket, and bind to chosen port
+    server_socket.bind(('',server_port))
+
+    # Configure how many requests can be queued on the server at once
+    server_socket.listen(2)
+
+    # Alert user we are now online
+    log.info("The server is ready to receive on port " + str(server_port))
+
+    # Surround with a try-finally to ensure we clean up the socket after we're done
+    try:
+        # Enter forever loop to listen for requests
+        while True:
+            # When each client connects, create references to their respective sockets and addresses
+            connection_socketX, addressX = server_socket.accept()
+            log.info("Connected to " + str(addressX) + "\nconnection_socket: " + str(connection_socketX))
+
+            connection_socketY, addressY = server_socket.accept()
+            log.info("Connected to " + str(addressY) + "\nconnection_socket: " + str(connection_socketY))
+
+            # TODO: Determine if address has already visited server
+
+            # implementation of threading for each client
+            threading.Thread(target=connection_handler, args=(connection_socketX, addressX)).start()
+            threading.Thread(target=connection_handler, args=(connection_socketY, addressY)).start()
+
+    finally:
+        server_socket.close()
+
+if __name__ == "__main__":
+    main()
